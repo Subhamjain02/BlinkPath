115e6da89f9226b506c54c1c124c8f63c9ed9736
AW-Rishit <rishitsinha@aviraworld.co.in>
2025-06-26 20:50:58 +0200

    Add Hero Text animation and fix Hero Video Container transition

--- profile-demo/src/Components/Hero/Hero.jsx
+++ profile-demo/src/Components/Hero/Hero.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import styles from './Hero.module.css'
 import Header from './Header'
 
+import DecryptedText from './DecryptedText';
+
 function Hero() {
   return (
     <div className={styles.hero}>
       <Header/>
-      <p className={styles.heroText}>
+      {/* <p className={styles.heroText}>
         We’re a design studio for product and brand teams who mean business.
-      </p>
+      </p> */}
+      <DecryptedText text="We’re a design studio for product and brand teams who mean business." 
+      speed={30}
+      maxIterations = {18}
+      sequential = {true}
+      revealDirection = {`start`}
+      useOriginalCharsOnly = {false}
+      class = {styles.textt}
+      />
+      
     </div>
   )
 }
